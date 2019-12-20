b3b223ce972977fe92025b19b042382a97b0f0a6
SeanFranklin <accounts@seanfranklin.ca>
2019-12-19 20:10:58 -0500

    Add files via upload

--- includes/database.php
+++ includes/database.php
@@ -1,13 +1,17 @@
 <?php
 /*******************************************************************************
 	Database connection constants for mysql
-	
+
 *******************************************************************************/
 
 	define("PRIMARY_DATABASE","ScorecardV5");
 	define("DATABASE_HOST","db");
 	define("DATABASE_USER","user");
 	define("DATABASE_PASSWORD","passw0rd");
+
+	define("HEMA_RATINGS_TOKEN","");
+	define("HEMA_RATINGS_BY_NAME","");
+	define("HEMA_RATINGS_BY_ID","");
 
 // END OF FILE /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
