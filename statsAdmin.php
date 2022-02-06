75ff1ca59988d47d2c484130a3661c0acb76ec23
SeanFranklin <accounts@seanfranklin.ca>
2022-02-05 20:44:31 -0500

    Add files via upload

--- statsAdmin.php
+++ statsAdmin.php
@@ -17,7 +17,7 @@ if(ALLOW['SOFTWARE_ASSIST'] == false){
 	pageError('user');
 } else {
 
-comparingRulesTypes();
+doublesByExchCount();
 
 }
 
@@ -25,6 +25,99 @@ include('includes/footer.php');
 
 // FUNCTIONS ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+
+/******************************************************************************/
+
+function doublesByExchCount(){
+
+	$eventID = 139;
+	$eventIDs = [31,99,86,6,3,14,35,41,173,158,171,17,47,170,68,104,46,102,176,110,19,123,65,28,7,21,131,11,13,25,51,139,89,27];
+	$eventIDs = implode2int($eventIDs);
+
+	$sql = "SELECT matchID, exchangeType
+			FROM eventExchanges
+			INNER JOIN eventMatches USING(matchID)
+			INNER JOIN eventGroups USING(groupID)
+			INNER JOIN eventTournaments USING(tournamentID)
+			WHERE eventID IN ($eventIDs)
+			AND tournamentWeaponID = 1
+			AND exchangeType IN('clean','double','afterblow','noExchange','noQuality')";
+	$exchanges = mysqlQuery($sql, ASSOC);
+
+	$matchID = 0;
+	$a['double'] = 0;
+	$a['other'] = 0;
+	$doubleCount = [$a,$a,$a,$a,$a,$a,$a];
+	$matchesAt = [-1,0,0,0,0,0,0];
+	$numDoubles = 0;
+	$numMatches = -1;
+	$numExchanges = 0;
+	$noExchCount = [0,0,0,0,0,0,0];
+	$numNoExchange = 0;
+	$scoringCount = [0,0,0,0,0,0,0];
+
+	foreach($exchanges as $exchange){
+
+		if($exchange['matchID'] != $matchID){
+			$matchID = $exchange['matchID'];
+			$matchesAt[$numDoubles]++;
+			$numDoubles = 0;
+			$numMatches++;
+		}
+		
+
+		if($exchange['exchangeType'] == 'double'){
+			$doubleCount[$numDoubles]['double']++;
+			$numDoubles++;
+			$numExchanges++;
+			$scoringCount[$numDoubles]++;
+		} elseif($exchange['exchangeType'] == 'noExchange' || $exchange['exchangeType'] == 'noQuality'){
+			$noExchCount[$numDoubles]++;
+			$numNoExchange++;
+		} elseif($exchange['exchangeType'] == 'clean' || $exchange['exchangeType'] == 'afterblow'){
+			$doubleCount[$numDoubles]['other']++;
+			$numExchanges++;
+			$scoringCount[$numDoubles]++;
+		} else {
+
+		}
+
+	}
+
+	$d0 = round(100 * $doubleCount[0]['double'] / ($scoringCount[0]),1);
+	$d1 = round(100 * $doubleCount[1]['double'] / ($scoringCount[1]),1);
+	$d2 = round(100 * $doubleCount[2]['double'] / ($scoringCount[2]),1);
+	$m0 = round(100 * $matchesAt[0] / ($numMatches),1);
+	$m1 = round(100 * $matchesAt[1] / ($numMatches),1);
+	$m2 = round(100 * $matchesAt[2] / ($numMatches),1);
+	$m3 = round(100 * $matchesAt[3] / ($numMatches),1);
+	$e0 = round(100 * $noExchCount[0] / ($scoringCount[0]),1);
+	$e1 = round(100 * $noExchCount[1] / ($scoringCount[1]),1);
+	$e2 = round(100 * $noExchCount[2] / ($scoringCount[2]),1);
+
+
+	echo "{$numExchanges} Scoring Exchanges<BR>";
+	echo "0 doubles: {$d0}% (".$doubleCount[0]['double']."/".$scoringCount[0].")<BR>";
+	echo "1 doubles: {$d1}% (".$doubleCount[1]['double']."/".$scoringCount[1].")<BR>";
+	echo "2 doubles: {$d2}% (".$doubleCount[2]['double']."/".$scoringCount[2].")<BR>";
+
+	echo "<HR>";
+
+	echo "{$numMatches} Matches<BR>";
+	echo "0 Doubles: {$m0}% (".$matchesAt[0].")<BR>";
+	echo "1 Doubles: {$m1}% (".$matchesAt[1].")<BR>";
+	echo "2 Doubles: {$m2}% (".$matchesAt[2].")<BR>";
+	echo "3 Doubles: {$m3}% (".$matchesAt[3].")<BR>";
+
+	echo "<HR>";
+
+	echo "{$numNoExchange} No Exchanges<BR>";
+	echo "0 Doubles: {$e0}% (".$noExchCount[0]."/".$scoringCount[0].")<BR>";
+	echo "1 Doubles: {$e1}% (".$noExchCount[1]."/".$scoringCount[1].")<BR>";
+	echo "2 Doubles: {$e2}% (".$noExchCount[2]."/".$scoringCount[2].")<BR>";
+
+}
+
 
 /******************************************************************************/
 
