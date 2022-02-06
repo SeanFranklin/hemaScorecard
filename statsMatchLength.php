c6706931b5797a6bddaf97284d232c436c1f2ca7
SeanFranklin <accounts@seanfranklin.ca>
2022-02-05 21:18:18 -0500

    Add files via upload

--- statsMatchLength.php
+++ statsMatchLength.php
@@ -15,12 +15,6 @@ include('includes/header.php');
 
 if(true){
 
-	define("DATA_SERIES_MAX",4);
-
-	if(!isset($_SESSION['StatsInfo']['displayType'])){
-		$_SESSION['StatsInfo']['displayType'] = 'percent';
-	}
-
 	$numDataSeries = countNumDataSeries();
 	$legend = [];
 	for($i = 0; $i < DATA_SERIES_MAX; $i++){
@@ -49,11 +43,6 @@ if(true){
 		$hasMatchTimers = true;
 	}
 
-	$tournamentList = getEventTournaments($_SESSION['eventID']);
-	$allTournaments = getSystemTournaments($_SESSION['eventID']);
-
-	$eventList = getEventList('matchesVisible',0,0,"eventName ASC, eventStartDate DESC");
-	
 // PAGE DISPLAY ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 ?>
@@ -78,20 +67,7 @@ if(true){
 
 <!-- Change Active Tournament ------------------------------------------------------------------------>
 
-	<form method='POST'>
-
-	<?php
-		for($i = 0; $i < DATA_SERIES_MAX; $i++){
-			//selectTournamentRow($tournamentList, $allTournaments, $i);
-			selectTournamentRow($eventList, $i);
-		}
-	?>
-
-	<button class='button' name='formName' value='updateActiveStatsItems'>
-		Update
-	</button>
-	
-	</form>
+	<?=selectDataSeriesTournaments()?>
 
 <!-- Display Graphs ------------------------------------------------------------------------>
 
@@ -186,119 +162,6 @@ include('includes/footer.php');
 
 // FUNCTIONS ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
-/******************************************************************************/
-
-function selectTournamentRow($eventList, $index){
-
-	if(isset($_SESSION['activeStatsItems']['tournamentIDs'][$index]) == true){
-		$activeTournamentID = (int)$_SESSION['activeStatsItems']['tournamentIDs'][$index];
-	} else {
-		$activeTournamentID = 0;
-	}
-
-	$activeEventID = (int)getTournamentEventID($activeTournamentID);
-
-	$tournamentIDs = (array)getEventTournaments($activeEventID);
-
-?>
-	<div>
-		<div class='cell input-group'>
-			<span class='input-group-label'>Event</span>
-
-			<select class='input-group-field' onchange="matchLengthEventSelect(<?=$index?>)"
-				id="match-length-event-<?=$index?>">
-				<option value='0' selected></option>
-				<?php foreach($eventList as $eventID => $event):?>
-					<option <?=optionValue($eventID, $activeEventID)?>>
-						<?=$event['eventName']?> <?=$event['eventYear']?>
-					</option>
-				<?php endforeach ?>
-			</select>
-
-			<span class='input-group-label'>Tournament <?=($index+1)?></span>
-
-			<select class='input-group-field' name="activeStatsItems[tournamentIDs][<?=$index?>]"
-				id="match-length-tournament-<?=$index?>">
-				<option value='0'></option>
-				<?php foreach($tournamentIDs as $tournamentID):?>
-					<option <?=optionValue($tournamentID, $activeTournamentID)?>>
-						<?=getTournamentName($tournamentID)?>
-					</option>
-				<?php endforeach ?>
-
-			</select>
-		</div>
-	</div>
-
-<?php
-}
-
-/******************************************************************************/
-
-function countNumDataSeries(){
-
-	$numDataSeries = 0;	if(isset($_SESSION['activeStatsItems']['tournamentIDs'][0]) == false || (int)$_SESSION['activeStatsItems']['tournamentIDs'][0] == 0){
-		$_SESSION['activeStatsItems']['tournamentIDs'][0] = (int)$_SESSION['tournamentID'];
-		if($_SESSION['activeStatsItems']['tournamentIDs'][0] != 0){
-			$numDataSeries++;
-		}
-
-	} else {
-		$numDataSeries++;
-	}
-
-	for($i = 1; $i < DATA_SERIES_MAX; $i++){
-		if(isset($_SESSION['activeStatsItems']['tournamentIDs'][$i]) == false){
-
-			$_SESSION['activeStatsItems']['tournamentIDs'][$i] = 0;
-
-		} else if($_SESSION['activeStatsItems']['tournamentIDs'][$i] != 0){
-
-			$numDataSeries++;
-		}
-	}
-
-	return $numDataSeries;
-}
-
-/******************************************************************************/
-
-function selectTournamentRow2($tournamentList, $allTournaments, $index){
-
-	if(isset($_SESSION['activeStatsItems']['tournamentIDs'][$index]) == true){
-		$activeTournamentID = (int)$_SESSION['activeStatsItems']['tournamentIDs'][$index];
-	} else {
-		$activeTournamentID = 0;
-	}
-?>
-	<div>
-		<div class='cell input-group'>
-			<span class='input-group-label'>Tournament <?=($index+1)?></span>
-
-			<select class='input-group-field' name="activeStatsItems[tournamentIDs][<?=$index?>][this]">
-				<option value='0' selected></option>
-				<?php foreach($tournamentList as $tournamentID):?>
-					<option <?=optionValue($tournamentID, $activeTournamentID)?>>
-						<?=getTournamentName($tournamentID)?>
-					</option>
-				<?php endforeach ?>
-			</select>
-
-			<select class='input-group-field' name="activeStatsItems[tournamentIDs][<?=$index?>][other]">
-				<option value='0'></option>
-					<?php foreach($allTournaments as $tournamentID => $tournament):?>
-						<option <?=optionValue($tournamentID, $activeTournamentID)?> >
-							<?=$tournament['eventName']?> [<?=$tournament['tournamentName']?>]
-						</option>
-					<?php endforeach ?>
-			</select>
-		</div>
-	</div>
-
-<?php
-}
-
 
 /******************************************************************************/
 
