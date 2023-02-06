6c51d6fc3100ff9ef1c671166f48b97952c262e6
SeanFranklin <accounts@seanfranklin.ca>
2023-02-05 20:07:38 -0500

    Add staffing roster to schedule block (#74)

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@
 
 include_once('includes/config.php');
 
-$vJ = '?=1.5.1'; // Javascript Version
+$vJ = '?=1.5.3'; // Javascript Version
 $vC = '?=1.2.1'; // CSS Version
 
 if(    ALLOW['EVENT_MANAGEMENT'] == true 
