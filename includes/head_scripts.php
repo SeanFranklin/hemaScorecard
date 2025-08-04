23222b3c6c3d7e987def1408cc2d268cd4e69e0c
Jamie Bliss <jamie@ivyleav.es>
2025-08-04 13:01:02 -0400

    Download dependencies

---
+++ includes/head_scripts.php
@@ -0,0 +1,8 @@
+    <link rel="stylesheet" href="includes/vendor/foundation.min.css">
+    <link rel="stylesheet" href="includes/vendor/dataTables.foundation.min.css">
+
+    <link rel="stylesheet" href="includes/vendor/chivo.css">
+    <link rel="stylesheet" href="includes/foundation/css/app.css">
+    <link rel="stylesheet" href="includes/foundation/css/custom.css<?=$vC?>">
+
+    <link rel='icon' href='includes/images/favicon.png'>
