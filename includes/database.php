13be3d85f74f8d7e39811392bb6e832f7957a150
pearsall-will <pearsallwilliam@gmail.com>
2026-06-14 18:33:40 -0400

    Updated docker compose and database.php

--- includes/database.php
+++ includes/database.php
@@ -3,16 +3,29 @@
 	Database connection constants for mysql
 
 *******************************************************************************/
+/* Load from environment */
+	$deployment = getenv('DEPLOYMENT') ?: DEPLOYMENT_UNKNOWN;
 
-	define("PRIMARY_DATABASE","ScorecardV5");
-	define("DATABASE_HOST","db");
-	define("DATABASE_USER","user");
-	define("DATABASE_PASSWORD","passw0rd");
-	define("DEPLOYMENT", DEPLOYMENT_UNKNOWN);
+	$database = getenv('PRIMARY_DATABASE') ?: 'ScorecardV5';
+	$db_host = getenv('DATABASE_HOST') ?: 'db';
+	$db_user = getenv('DATABASE_USER') ?: 'user';
+	$db_pwd = getenv('DATABASE_PASSWORD') ?: 'passw0rd';
+	
+	$hr_token = getenv('HEMA_RATINGS_TOKEN') ?: '';
+	$hr_by_name = getenv('HEMA_RATINGS_BY_NAME') ?: '';
+	$hr_by_id = getenv('HEMA_RATINGS_BY_ID') ?: '';
 
-	define("HEMA_RATINGS_TOKEN","");
-	define("HEMA_RATINGS_BY_NAME","");
-	define("HEMA_RATINGS_BY_ID","");
+/* Defnitions */
+	define("DEPLOYMENT", $deployment);
+
+	define("PRIMARY_DATABASE", $database);
+	define("DATABASE_HOST", $db_host);
+	define("DATABASE_USER", $db_user);
+	define("DATABASE_PASSWORD", $db_pwd);
+
+	define("HEMA_RATINGS_TOKEN", $hr_token);
+	define("HEMA_RATINGS_BY_NAME", $hr_by_name);
+	define("HEMA_RATINGS_BY_ID", $hr_by_id);
 
 // END OF FILE /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
